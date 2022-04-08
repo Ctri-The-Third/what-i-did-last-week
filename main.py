6ddcaf31b217f71115b8077bee3eaaa2d93cf133
C'tri <ctricoding@ctri.co.uk>
2022-04-09 00:38:15 +0100

    zendesk implementation & tests

---
+++ main.py
@@ -0,0 +1,18 @@
+from configparser import ConfigParser
+from src.controller import Controller, WorkItem
+
+if __name__ == "__main__":
+    con = Controller()
+
+    cfg = ConfigParser()
+    cfg.read_file(open("last-week.cfg", "r", encoding="utf-8"))
+    target = cfg.get("DEFAULT", "user_email")
+    con.zen_assignee = target
+    con.fetch_tasks()
+
+    for log in con.work_items:
+        log: WorkItem
+        if log.source != current_section:
+            print(f"--------------{log.source}-------------")
+            current_section = log.source
+            print(log)
